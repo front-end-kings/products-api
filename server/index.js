a93437b2d207bd6e1d02e10d4e08a9edfe2b9a4a
Ubuntu <ubuntu@ip-172-31-3-48.us-west-1.compute.internal>
2021-05-13 19:25:25 +0000

    changes from ubuntu

--- server/index.js
+++ server/index.js
@@ -3,14 +3,20 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const PORT = 3000;
+const compression = require('compression');
 const router = require('./router.js');
-require('newrelic');
+const config = require('../config/config.js');
 
+app.use(compression());
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded( { extended: true } ));
 app.use('/api', router);
+app.get('/loaderio-b6329a83a1f7b489e37b0eb235da990e', (req, res) => {
+console.log('load io received');
+  res.send(`${config.loaderio}`);
+});
 
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT}`);
